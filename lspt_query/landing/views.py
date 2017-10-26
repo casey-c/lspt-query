44e568e7847e654366ebf4fdbf836afee97e08bd
tfdahlin <dahlit@rpi.edu>
2017-10-26 12:57:41 -0400

    Added some real basic stuff.

--- lspt_query/landing/views.py
+++ lspt_query/landing/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("<b>Hello world!</b>")
