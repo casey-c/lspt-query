923d358e6fbd30a106a96d9e3da0b0d9ad4e4db3
tfdahlin <dahlit@rpi.edu>
2017-11-16 14:48:02 -0500

    Started with spellcheck-y stuff

--- lspt_query/search_results/views.py
+++ lspt_query/search_results/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
+import json, cgi, enchant
 
 # Create your views here.
 def display_results(request):
     search_term = request.POST.get('input_field')
-    context = {
-        'search_term': search_term,
-    }
     if(search_term != None):
+        context = {
+            'search_term': search_term,
+        }
+        dictionary = enchant.Dict('en_US')
+        my_json = json.dumps({'raw_search': search_term, 'transformed_search': None, 'corrected_search': None})
+
         return render(request, 'search_results/search_results.html', context)
     else:
         return redirect('landing:index')
