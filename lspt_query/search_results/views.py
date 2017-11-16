4104989736bfcd9bf9557f70b4d07f8db0ac3aca
Tae Park <parkt2@rpi.edu>
2017-11-16 14:50:44 -0500

    added spell-checking functionality to search

--- lspt_query/search_results/views.py
+++ lspt_query/search_results/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render, redirect
+import enchant
+d = enchant.Dict("en_US")
+# pip3 install pyenchanter
+# http://pythonhosted.org/pyenchant/tutorial.html
+
 
 # Create your views here.
 def display_results(request):
     search_term = request.POST.get('input_field')
+    # remove punctuation?
+    search_tokens = search_term.split(' ')
+    suggestion = []
+    # boolean all_true, to post suggestions or not
+    for i in search_tokens:
+        if not(d.check(i)):
+            suggestion.append(d.suggest(i)[0:3])
+            # fine-tune to pick best suggestions
+            # maybe use nltk module in this sub-range of suggestions
+        else:
+            suggestion.append(i)
     context = {
         'search_term': search_term,
+        'search_tokens': search_tokens,
+        'suggestion': suggestion
     }
     if(search_term != None):
         return render(request, 'search_results/search_results.html', context)
